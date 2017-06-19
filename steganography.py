e32dc86d55afa9c0ff2b020e74c02d41f8b8adff
nizi2734 <Nicholas.Zimmerer@lasp.colorado.edu>
2017-06-19 17:22:33 -0600

    Modularized steganograpy.py for library-like useage

--- steganography.py
+++ steganography.py
@@ -22,47 +22,10 @@ def main():
     parser.add_argument("-v", "--verbose", help="Enable verbose output", action="store_true")
     args = parser.parse_args()
 
-    # Supported image types
-    if imghdr.what(args.input_image) != "png":
-        print("Sorry, but this program only supports .png image formats")
-        exit(1)
-
-    # Prime input image
-    image = Image.open(args.input_image)
-
-    ###############################################################################
-    # Encoding message into image
-    ###############################################################################
     if args.message:
-
-        if args.verbose: print("Converting \"{}\" to binary...".format(args.message))
-        binaryMessage = toBinary(args.message)[2:]
-
-        # Check that picture is large enough to contain message
-        image = Image.open(args.input_image)
-
-        if len(binaryMessage) > (image.size[0] * image.size[1] * 3):
-            print("This image is too small to contain your message!\nExiting...")
-            exit(2)
-
-        # Encode image with message data
-        encodedPixelData = encodeMessage(image, binaryMessage, image.size, args.verbose)
-
-        # Create new image file
-        savedImage = Image.frombytes('RGB', image.size, bytes(encodedPixelData))
-        savedImage.save(args.output_filename + '.png', 'PNG')
-        if args.verbose: print("Saved encoded data as \"{}.png\"".format(args.output_filename))
-
-    ###############################################################################
-    # Decoding message from image
-    ###############################################################################
+        encodeMessage(args.input_image, args.output_filename, args.message, args.verbose)
     elif args.decode:
-        byteList = extractMessage(image, image.size, args.verbose)
-        message = "" 
-        if args.verbose: print("Converting binary to text...")
-        for byte in byteList:
-            message += toText(byte)
-        print("\n{}\n\nDone".format(message))
+        extractMessage(args.input_image, args.verbose)
 
 
 def toBinary(string):
@@ -89,24 +52,38 @@ def toText(binaryMessage):
         exit(3)
 
 
-def encodeMessage(image, binaryMessage, size, verbose):
+def encodeMessage(imageFile, outputFile, message, verbose=False):
     ''' Unpacks image, and encodes the message in LSB format
 
         Input: image, message in binary, image width, image height, and verbose flag
         Output: list of pixel values
     '''
-    if verbose: print("Encoding message...")
-
-    # How much of message has been encoded
+    # Local variable declarations
     bitsInjected = 0
     delimBits = 0
-
-    # Byte buffer for pixel data
     imageData = []
 
+    # Check image format 
+    if imghdr.what(imageFile) != "png":
+        print("Sorry, but this program only supports .png image formats")
+        exit(1)
+
+    # Open input image
+    image = Image.open(imageFile)
+
+    if verbose: print("Converting \"{}\" to binary...".format(message))
+
+    # Check message length
+    binaryMessage = toBinary(message)[2:]
+    if len(binaryMessage) > (image.size[0] * image.size[1] * 3):
+        print("This image is too small to contain your message!\nExiting...")
+        exit(2)
+
+    if verbose: print("Encoding message...")
+
     # Loop through every pixel of input image
-    for y in range(size[1]):
-        for x in range(size[0]):
+    for y in range(image.size[1]):
+        for x in range(image.size[0]):
             for index, color in enumerate(image.getpixel((x,y))):
 
                 # Encode one bit of the message string
@@ -123,15 +100,39 @@ def encodeMessage(image, binaryMessage, size, verbose):
                 else:
                     imageData.append(color)
 
-    return imageData
+    # Create new image file
+    savedImage = Image.frombytes('RGB', image.size, bytes(imageData))
+    savedImage.save(outputFile + '.png', 'PNG')
+    if verbose: print("Saved encoded data as \"{}.png\"\nDone".format(outputFile))
 
-def extractMessage(image, size, verbose):
+
+def extractMessage(imageFile, verbose=False):
     ''' Extracts message from image
 
         Input: <input_image>, <image_size>, <verbosity_flag>
         Output: <message>
     '''
+    # Check image format 
+    if imghdr.what(imageFile) != "png":
+        print("Sorry, but this program only supports .png image formats")
+        exit(1)
+
+    # Open input image
+    image = Image.open(imageFile)
+
     if verbose: print("Extracting message...")
+
+    byteList = getBinaryMessage(image)
+
+    if verbose: print("Converting binary to text...")
+
+    message = ""
+    for byte in byteList:
+        message += toText(byte)
+    print("\n{}\n\nDone".format(message))
+
+
+def getBinaryMessage(image):
 
     # Buffer to store message
     message = "0b0"
@@ -144,8 +145,8 @@ def extractMessage(image, size, verbose):
     byte = ""
 
     # Loop through picture and store all LSB in message
-    for y in range(size[1]):
-        for x in range(size[0]):
+    for y in range(image.size[1]):
+        for x in range(image.size[0]):
             for color in image.getpixel((x,y)):
 
                 # One bit of the message
@@ -162,9 +163,7 @@ def extractMessage(image, size, verbose):
                     byteList.append(byte)
                     bitCount = 0
                     byte = ""
-                    
-    return byteList
-    
+
 
 if __name__ == "__main__":
     main()
